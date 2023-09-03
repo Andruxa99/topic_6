c29339be74fc13c43db54340ef502ffc1564898e
Andruxa99 <andrei1999311@gmail.com>
2023-09-03 18:05:16 +0300

    Домашнее задание по теме "Условные операторы"

--- tasks/task_4.py
+++ tasks/task_4.py
@@ -1,2 +1,13 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+number_1: int = int(input("Введите первое число: "))
+number_2: int = int(input("Введите второе число: "))
+number_3: int = int(input("Введите третье число: "))
+
+if number_1 == number_2 and number_1 == number_3:
+    print("Все числа равны")
+elif number_1 > number_2 and number_1 > number_3:
+    print("Наибольшее число:", number_1)
+elif number_2 > number_3:
+    print("Наибольшее число:", number_2)
+elif number_3:
+    print("Наибольшее число:", number_3)
 
